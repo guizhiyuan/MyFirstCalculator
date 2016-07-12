178249e6035a78c609bfd4ff3ade9c14f36e0199
guizhiyuan <guizhiyuan@outlook.com>
2016-07-12 22:18:20 +0800

    Initial commit

---
+++ app/src/main/java/calculator/guizhiyuan/com/myfirstcalculator/MainActivity.java
@@ -0,0 +1,18 @@
+package calculator.guizhiyuan.com.myfirstcalculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button btn=(Button)findViewById(R.id.btn_equal);
+        btn.setOnClickListener(new View.OnClickListener());
+
+    }
+}
